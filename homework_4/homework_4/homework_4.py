c1a6cbb0ba8f73834582bb0c8ea76e54b5d9a2b1
Дмитрий Ткаченко <st086996@student.spbu.ru>
2023-04-25 11:18:53 +0300

    fix 2

--- homework_4/homework_4/homework_4.py
+++ homework_4/homework_4/homework_4.py
@@ -1,9 +1,9 @@
-# -*- coding: cp1251 -*-
+п»ї# -*- coding: cp1251 -*-
 
 import sympy
 from sympy import sin, cos ,exp, integrate,sympify
 from sympy.abc import x
-def function(f,y):# Значение функции в точке Sx:
+def function(f,y):# Р—РЅР°С‡РµРЅРёРµ С„СѓРЅРєС†РёРё РІ С‚РѕС‡РєРµ Sx:
     return f.evalf(subs = {x : y})
 def MAX_f(f ,a,b): 
     h = 1/100
@@ -24,13 +24,13 @@ def integrate_KF(list_f):
     for f in list_f :  
             F = integrate(f,x) 
             print("_______________________________________")
-            print("Точное значение интеграла для функции \n    ",f," \nНа промежутке [",a,",",b,"]    =   ", function(F,b) - function(F,a))  
-            print("Приблизительное значение интеграла при помощи ..... Абсолютная погрешность составляет \n")
-            print("КФ левого прямоугольника = ", (b-a)*function(f,a) , " -  ",abs((b-a)*function(f,a)-(function(F,b) - function(F,a))) )
-            print("правого прямоугольника   = ", (b-a)*function(f,b), " -  ",abs((b-a)*function(f,b)-(function(F,b) - function(F,a))) )
-            print("среднего прямоугольника  = ", (b-a)*function(f,(a+b)/2), " -  ",abs((b-a)*function(f,(a+b)/2)-(function(F,b) - function(F,a))))
-            print("трапеции                 = ", (b-a)/2*(function(f,a) + function(f,b)), " -  ",abs((b-a)/2*(function(f,a) + function(f,b))-(function(F,b) - function(F,a))) ) 
-            print("Симпсона                 = ", (b-a)/6*(function(f,a) + function(f,b) +4*function(f,(a+b)/2)), " -   ",abs((b-a)/6*(function(f,a) + function(f,b) +4*function(f,(a+b)/2))-(function(F,b) - function(F,a))) )
+            print("РўРѕС‡РЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РґР»СЏ С„СѓРЅРєС†РёРё \n    ",f," \nРќР° РїСЂРѕРјРµР¶СѓС‚РєРµ [",a,",",b,"]    =   ", function(F,b) - function(F,a))  
+            print("РџСЂРёР±Р»РёР·РёС‚РµР»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РїСЂРё РїРѕРјРѕС‰Рё ..... РђР±СЃРѕР»СЋС‚РЅР°СЏ РїРѕРіСЂРµС€РЅРѕСЃС‚СЊ СЃРѕСЃС‚Р°РІР»СЏРµС‚ \n")
+            print("РљР¤ Р»РµРІРѕРіРѕ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР° = ", (b-a)*function(f,a) , " -  ",abs((b-a)*function(f,a)-(function(F,b) - function(F,a))) )
+            print("РїСЂР°РІРѕРіРѕ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°   = ", (b-a)*function(f,b), " -  ",abs((b-a)*function(f,b)-(function(F,b) - function(F,a))) )
+            print("СЃСЂРµРґРЅРµРіРѕ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°  = ", (b-a)*function(f,(a+b)/2), " -  ",abs((b-a)*function(f,(a+b)/2)-(function(F,b) - function(F,a))))
+            print("С‚СЂР°РїРµС†РёРё                 = ", (b-a)/2*(function(f,a) + function(f,b)), " -  ",abs((b-a)/2*(function(f,a) + function(f,b))-(function(F,b) - function(F,a))) ) 
+            print("РЎРёРјРїСЃРѕРЅР°                 = ", (b-a)/6*(function(f,a) + function(f,b) +4*function(f,(a+b)/2)), " -   ",abs((b-a)/6*(function(f,a) + function(f,b) +4*function(f,(a+b)/2))-(function(F,b) - function(F,a))) )
             print("3/8                      = ", (b-a)/8*(function(f,a) + function(f,b) +3*function(f,a+(b-a)/3)+3*function(f,a+2*(b-a)/3)), " -  ",abs(-(function(F,b) - function(F,a))+ (b-a)/8*(function(f,a) + function(f,b) +3*function(f,a+(b-a)/3)+3*function(f,a+2*(b-a)/3))) )
             print("_______________________________________")
 def SKF_Left_triangle(f,m, a ,b ):
@@ -40,8 +40,8 @@ def SKF_Left_triangle(f,m, a ,b ):
     for j in range(0,m): 
         result += function(f,a+j*h)
     result *= h 
-    print("CКФ Левого треугольника  = ", result , " - абс.погр. - ",abs(result-(function(F,b) - function(F,a))), end = '' )
-    print("   Теор.погр. = ", 1/2*(b-a)*h**1*MAX_f(f_diff(f,1),a,b))
+    print("CРљР¤ Р›РµРІРѕРіРѕ С‚СЂРµСѓРіРѕР»СЊРЅРёРєР°  = ", result , " - Р°Р±СЃ.РїРѕРіСЂ. - ",abs(result-(function(F,b) - function(F,a))), end = '' )
+    print("   РўРµРѕСЂ.РїРѕРіСЂ. = ", 1/2*(b-a)*h**1*MAX_f(f_diff(f,1),a,b))
     return result 
 def SKF_Right_triangle(f,m, a ,b ): 
     F = integrate(f,x)
@@ -50,8 +50,8 @@ def SKF_Right_triangle(f,m, a ,b ):
     for j in range(0,m): 
         result += function(f,a+(j+1)*h)
     result *= h 
-    print("CКФ правого треугольника  = ", result , " - абс.погр. - ",abs(result-(function(F,b) - function(F,a))) , end = '' )
-    print("   Теор.погр. = ", 1/2*(b-a)*h**1*MAX_f(f_diff(f,1),a,b))
+    print("CРљР¤ РїСЂР°РІРѕРіРѕ С‚СЂРµСѓРіРѕР»СЊРЅРёРєР°  = ", result , " - Р°Р±СЃ.РїРѕРіСЂ. - ",abs(result-(function(F,b) - function(F,a))) , end = '' )
+    print("   РўРµРѕСЂ.РїРѕРіСЂ. = ", 1/2*(b-a)*h**1*MAX_f(f_diff(f,1),a,b))
      
     return result 
 def SKF_Middle_triangle(f,m, a ,b ): 
@@ -61,8 +61,8 @@ def SKF_Middle_triangle(f,m, a ,b ):
     for j in range(0,m): 
         result += function(f,a+(j+1/2)*h)
     result *= h 
-    print("CКФ среднего треугольника = ", result , " - абс.погр. - ",abs(result-(function(F,b) - function(F,a))) , end = '')
-    print("   Теор.погр. = ", 1/24*(b-a)*h**2*MAX_f(f_diff(f,2),a,b))
+    print("CРљР¤ СЃСЂРµРґРЅРµРіРѕ С‚СЂРµСѓРіРѕР»СЊРЅРёРєР° = ", result , " - Р°Р±СЃ.РїРѕРіСЂ. - ",abs(result-(function(F,b) - function(F,a))) , end = '')
+    print("   РўРµРѕСЂ.РїРѕРіСЂ. = ", 1/24*(b-a)*h**2*MAX_f(f_diff(f,2),a,b))
     return result 
 def SKF_Trapezoid(f,m, a ,b ): 
     F = integrate(f,x)
@@ -71,8 +71,8 @@ def SKF_Trapezoid(f,m, a ,b ):
     for j in range(0,m): 
         result += (function(f,a+j*h) + function(f,a+(j+1)*h))/2
     result *= h
-    print("CКФ трапеции              = ", result , " - абс.погр. - ",abs(result-(function(F,b) - function(F,a))) , end = '')
-    print("   Теор.погр. = ", 1/12*(b-a)*h**2*MAX_f(f_diff(f,2),a,b))
+    print("CРљР¤ С‚СЂР°РїРµС†РёРё              = ", result , " - Р°Р±СЃ.РїРѕРіСЂ. - ",abs(result-(function(F,b) - function(F,a))) , end = '')
+    print("   РўРµРѕСЂ.РїРѕРіСЂ. = ", 1/12*(b-a)*h**2*MAX_f(f_diff(f,2),a,b))
     return result 
 def SKF_Simpson(f,m, a ,b ): 
     F = integrate(f,x)
@@ -81,16 +81,16 @@ def SKF_Simpson(f,m, a ,b ):
     for j in range(0,m): 
         result += function(f,a+j*h) + 4*function(f,a+(j+1/2)*h) + function(f,a+(j+1)*h)
     result *= h/6
-    print("CКФ симпсона              = ", result , " - абс.погр. - ",abs(result-(function(F,b) - function(F,a))) , end = '')
-    print("   Теор.погр. = ", 1/2880*(b-a)*h**3*MAX_f(f_diff(f,3),a,b))
+    print("CРљР¤ СЃРёРјРїСЃРѕРЅР°              = ", result , " - Р°Р±СЃ.РїРѕРіСЂ. - ",abs(result-(function(F,b) - function(F,a))) , end = '')
+    print("   РўРµРѕСЂ.РїРѕРіСЂ. = ", 1/2880*(b-a)*h**3*MAX_f(f_diff(f,3),a,b))
     return result 
 def integrate_SKF(list_f):
-    m = int(input("Введите число промежутков деления "))
+    m = int(input("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РїСЂРѕРјРµР¶СѓС‚РєРѕРІ РґРµР»РµРЅРёСЏ "))
     list_SKF = [SKF_Left_triangle ,SKF_Right_triangle,SKF_Middle_triangle,SKF_Trapezoid,SKF_Simpson]
     for f in list_f :  
         F = integrate(f,x) 
         print("_______________________________________")
-        print("Точное значение интеграла для функции \n  ",f," \nНа промежутке [",a,",",b,"]    =   ", function(F,b) - function(F,a))
+        print("РўРѕС‡РЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РґР»СЏ С„СѓРЅРєС†РёРё \n  ",f," \nРќР° РїСЂРѕРјРµР¶СѓС‚РєРµ [",a,",",b,"]    =   ", function(F,b) - function(F,a))
         print("_______________________________________") 
         for k in list_SKF: 
             k(f,m ,a,b) 
@@ -104,15 +104,15 @@ a,b = 0,2
 list_f = [x**0, x**1 ,x**2, x**3, x**4, x**3+sin(x)]
 i = 1
 '''while(i != '0'):
-    print("Введите функцию для интегрирований(по x) , по окончании вывода введите 0")
+    print("Р’РІРµРґРёС‚Рµ С„СѓРЅРєС†РёСЋ РґР»СЏ РёРЅС‚РµРіСЂРёСЂРѕРІР°РЅРёР№(РїРѕ x) , РїРѕ РѕРєРѕРЅС‡Р°РЅРёРё РІС‹РІРѕРґР° РІРІРµРґРёС‚Рµ 0")
     i = input()
     if(i != '0') :
         list_f.append(sympify(i)) '''
 choise = 0 
 while(choise != '0') :
     i =  1 
-    print("Меню  \n 1 - КФ \n 2 - СКФ \n 3 - дополнить список интегрируемых функций ")
-    print(" 4 - отобразить список интегрируемых функций и пределы интегрирования   \n 5 - изменить пределы интегрирования \n 0 - завершить работу программмы")
+    print("РњРµРЅСЋ  \n 1 - РљР¤ \n 2 - РЎРљР¤ \n 3 - РґРѕРїРѕР»РЅРёС‚СЊ СЃРїРёСЃРѕРє РёРЅС‚РµРіСЂРёСЂСѓРµРјС‹С… С„СѓРЅРєС†РёР№ ")
+    print(" 4 - РѕС‚РѕР±СЂР°Р·РёС‚СЊ СЃРїРёСЃРѕРє РёРЅС‚РµРіСЂРёСЂСѓРµРјС‹С… С„СѓРЅРєС†РёР№ Рё РїСЂРµРґРµР»С‹ РёРЅС‚РµРіСЂРёСЂРѕРІР°РЅРёСЏ   \n 5 - РёР·РјРµРЅРёС‚СЊ РїСЂРµРґРµР»С‹ РёРЅС‚РµРіСЂРёСЂРѕРІР°РЅРёСЏ \n 0 - Р·Р°РІРµСЂС€РёС‚СЊ СЂР°Р±РѕС‚Сѓ РїСЂРѕРіСЂР°РјРјРјС‹")
     choise = int(input())
     if choise == 0 : 
         break 
@@ -122,7 +122,7 @@ while(choise != '0') :
         integrate_SKF(list_f)
     elif choise == 3 : 
         while(i != '0'):
-            print("Введите функцию для интегрирований(по x) , по окончании вывода введите 0")
+            print("Р’РІРµРґРёС‚Рµ С„СѓРЅРєС†РёСЋ РґР»СЏ РёРЅС‚РµРіСЂРёСЂРѕРІР°РЅРёР№(РїРѕ x) , РїРѕ РѕРєРѕРЅС‡Р°РЅРёРё РІС‹РІРѕРґР° РІРІРµРґРёС‚Рµ 0")
             i = input()
             if(i != '0') :
                 list_f.append(sympify(i)) 
@@ -130,8 +130,8 @@ while(choise != '0') :
         print(list_f) 
         print("[" , a ," , " ,b, "]")
     elif choise == 5 : 
-        a = float(input("Введите левый предел\n"))
-        b = float(input("Введите правый предел\n"))
-        print("Готово, пределы изменены")
+        a = float(input("Р’РІРµРґРёС‚Рµ Р»РµРІС‹Р№ РїСЂРµРґРµР»\n"))
+        b = float(input("Р’РІРµРґРёС‚Рµ РїСЂР°РІС‹Р№ РїСЂРµРґРµР»\n"))
+        print("Р“РѕС‚РѕРІРѕ, РїСЂРµРґРµР»С‹ РёР·РјРµРЅРµРЅС‹")
 
 
